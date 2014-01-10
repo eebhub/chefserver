84532a7cb86eb266cbb517a13c963210954611f9
Josh Wentz <joshwentz@gmail.com>
2014-01-10 12:57:57 -0500

    NREL OpenStudio Chef cookbook

---
+++ cookbooks/openstudio/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:openstudio][:version] = "1.0.2"
+default[:openstudio][:version_revision] = "efb742a74c"
+default[:openstudio][:platform] = "Linux"
+default[:openstudio][:download_url] = "http://developer.nrel.gov/downloads/buildings/openstudio/builds"
